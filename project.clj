b27ecfbe46c1058666aad49c056f518acbe65ee9
Christopher Mark Gore <cgore@cgore.com>
2015-07-07 15:01:16 -0500

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject tonic "0.1.0-SNAPSHOT"
-  :description "All of the schemata you need and/or want."
-  :url "https://github.com/cgore/gin"
+(defproject tonic "0.1.0"
+  :description "All of the schemata (\"schemas\") you need and/or want."
+  :url "https://github.com/cgore/tonic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
