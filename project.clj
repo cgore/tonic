215fc18f3f59363b7c17db205560267102925a8e
Christopher Mark Gore <cgore@cgore.com>
2016-01-11 14:13:09 -0600

    Updating schema version.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject tonic "0.3.3"
+(defproject tonic "0.3.4"
   :description "All of the schemata (\"schemas\") you need and/or want."
   :url "https://github.com/cgore/tonic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [prismatic/schema "1.0.1"]])
+                 [prismatic/schema "1.0.4"]])
