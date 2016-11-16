052a3ed6a0dbba5dddc90ef76504313686024c0a
Christopher Mark Gore <cgore@cgore.com>
2016-11-16 13:21:57 -0600

    version bump

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/cgore/tonic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [prismatic/schema "1.0.4"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [prismatic/schema "1.1.3"]])
