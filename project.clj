74492cabc5dc97c72893b5aba5a052659023eb31
Christopher Mark Gore <cgore@cgore.com>
2017-11-13 13:12:06 -0600

    schema v1.1.7

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [prismatic/schema "1.1.3"]])
+                 [prismatic/schema "1.1.7"]])
